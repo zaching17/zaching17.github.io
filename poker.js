9aedbf03376e1bf5ca6151906b72e8d1888290a0
zaching17 <159203456+zaching17@users.noreply.github.com>
2024-03-01 16:24:25 -0500

    Add files via upload

---
+++ poker.js
@@ -0,0 +1,19 @@
+var yourPoints = 0;
+var housePoints = 0;
+
+window.onload= function() {
+    buildDeck();
+}
+
+function buildDeck() {
+    let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let face = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]);
+        }
+    }
+    console.log(deck)
+}
